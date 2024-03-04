f1373677bb4fbb3b8127bc39bd7b6298b9e03977
ramshukla67 <ramshukla67@gmail.com>
2024-03-05 00:46:43 +0530

    Fix ssh issue

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instance_1" {
   ami           = "ami-052c9ea013e6e3567"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
-
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
@@ -18,7 +18,7 @@ resource "aws_instance" "ec2_instance_2" {
   ami           = "ami-052c9ea013e6e3567"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_2.id
-
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
--- security_groups.tf
+++ security_groups.tf
@@ -9,7 +9,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
   }
-
+  ingress {
+	from_port   = 22
+    to_port     = 22
+	protocol    = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
