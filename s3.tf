7f9842cbe132090d445cc6c4fd48b2a3d0f5dc2c
ramshukla67 <ramshukla67@gmail.com>
2024-03-05 17:23:18 +0530

    5th Itter Checkov fixes

--- s3.tf
+++ s3.tf
@@ -46,9 +46,11 @@ resource "aws_s3_bucket_notification" "bucket-notification" {
 }
 resource "aws_kms_key" "mykey" {
 	 description = "KMS key 1"
+	 is_enabled             = true
+	 enable_key_rotation    = true
 }
 
-resource "aws_s3_bucket_server_side_encription_configuration" "sse_good" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_good" {
 	bucket = aws_s3_bucket.terraform_state.bucket
 	rule {
 		apply_server_side_encryption_by_default {
@@ -59,7 +61,9 @@ resource "aws_s3_bucket_server_side_encription_configuration" "sse_good" {
 }
 resource "aws_s3_bucket" "replica" {
   bucket = "replica-terraform-s3-versioning"
-  
+  versioning {
+    enabled = true
+  }
   replication_configuration {
     role = aws_iam_role.replication.arn
   
@@ -76,6 +80,18 @@ resource "aws_s3_bucket" "replica" {
           status = "Enabled"
         }
       }
+	  lifecycle_rule {
+		id = "expire"
+		status = "Enabled"
+		prefix = "logs/"
+		tranition {
+			days = 30
+			storage_class = "STANDARD_IA"
+		}
+		expiration{
+			days = 10
+		}
+	  }
     }
   }
 }
@@ -103,4 +119,34 @@ EOF
 resource "aws_iam_role_policy_attachment" "replication" {
   role       = aws_iam_role.replication.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.example.id
+  target_bucket = aws_s3_bucket.replica.id
+  target_prefix = "log/"
+}
+resource "aws_s3_bucket_public_access_block" "access_replication_state" {
+  bucket = aws_s3_bucket.replica.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls=true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_notification" "bucket-notification" {
+	bucket = aws_s3_bucket.replica.id
+	topic {
+		topic_arn = aws_sns_topic.bucket-notifications.arn
+		event = ["s3:ObjectCreated:*"]
+		filter_prefix = "logs/"
+	}
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
+  bucket = aws_s3_bucket.replica.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
--- security_groups.tf
+++ security_groups.tf
@@ -18,3 +18,6 @@ resource "aws_security_group" "ec2_sg" {
 	description = "Enables SSH port"
   }
 }
+resource "aws_default_security_group" "default" {
+	vpc_id = aws_vpc.issue_vpc.id
+}
